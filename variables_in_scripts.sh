adbe1841e0cd8ae297febf6bf2977f9487b1d33e
Jakubsoad <jakub.soad@gmail.com>
2021-12-23 15:19:57 +0100

    Learning the variables

---
+++ variables_in_scripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+raport_creator="Jakub Mroz"
+raport_name="Performance"
+
+db_name="raports"
+db_user="jmroz"
+db_pass="password"
+
+start_bash=`date`
+
+echo "********************************"
+echo "Raport's title: $raport_name - created $start_bash"
+echo "********************************"
